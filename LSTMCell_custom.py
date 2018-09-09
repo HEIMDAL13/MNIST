6224b29a8359d1899570837e3072478e665c34d5
HEIMDAL13 <vlcl_ori_13@hotmail.com>
2018-09-09 15:19:12 +0200

    added gate output visualization

---
+++ LSTMCell_custom.py
@@ -0,0 +1,102 @@
+from torch.nn.modules.rnn import RNNCellBase
+import math
+import torch
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+from torch.nn._functions.thnn import rnnFusedPointwise as fusedBackend
+class LSTMCell_custom(RNNCellBase):
+    r"""A long short-term memory (LSTM) cell.
+
+    .. math::
+
+        \begin{array}{ll}
+        i = \sigma(W_{ii} x + b_{ii} + W_{hi} h + b_{hi}) \\
+        f = \sigma(W_{if} x + b_{if} + W_{hf} h + b_{hf}) \\
+        g = \tanh(W_{ig} x + b_{ig} + W_{hc} h + b_{hg}) \\
+        o = \sigma(W_{io} x + b_{io} + W_{ho} h + b_{ho}) \\
+        c' = f * c + i * g \\
+        h' = o \tanh(c') \\
+        \end{array}
+
+    where :math:`\sigma` is the sigmoid function.
+
+    Args:
+        input_size: The number of expected features in the input `x`
+        hidden_size: The number of features in the hidden state `h`
+        bias: If `False`, then the layer does not use bias weights `b_ih` and
+            `b_hh`. Default: ``True``
+
+    Inputs: input, (h_0, c_0)
+        - **input** of shape `(batch, input_size)`: tensor containing input features
+        - **h_0** of shape `(batch, hidden_size)`: tensor containing the initial hidden
+          state for each element in the batch.
+        - **c_0** of shape `(batch, hidden_size)`: tensor containing the initial cell state
+          for each element in the batch.
+
+          If `(h_0, c_0)` is not provided, both **h_0** and **c_0** default to zero.
+
+    Outputs: h_1, c_1
+        - **h_1** of shape `(batch, hidden_size)`: tensor containing the next hidden state
+          for each element in the batch
+        - **c_1** of shape `(batch, hidden_size)`: tensor containing the next cell state
+          for each element in the batch
+
+    Attributes:
+        weight_ih: the learnable input-hidden weights, of shape
+            `(4*hidden_size x input_size)`
+        weight_hh: the learnable hidden-hidden weights, of shape
+            `(4*hidden_size x hidden_size)`
+        bias_ih: the learnable input-hidden bias, of shape `(4*hidden_size)`
+        bias_hh: the learnable hidden-hidden bias, of shape `(4*hidden_size)`
+    """
+
+    def __init__(self, input_size, hidden_size, bias=True, print=False):
+        super(LSTMCell_custom, self).__init__()
+        self.print = print
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.weight_ih = Parameter(torch.Tensor(4 * hidden_size, input_size))
+        self.weight_hh = Parameter(torch.Tensor(4 * hidden_size, hidden_size))
+        if bias:
+            self.bias_ih = Parameter(torch.Tensor(4 * hidden_size))
+            self.bias_hh = Parameter(torch.Tensor(4 * hidden_size))
+        else:
+            self.register_parameter('bias_ih', None)
+            self.register_parameter('bias_hh', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, hx):
+        self.check_forward_input(input)
+        self.check_forward_hidden(input, hx[0], '[0]')
+        self.check_forward_hidden(input, hx[1], '[1]')
+        return self.forward_function(input, hx,self.weight_ih, self.weight_hh, self.bias_ih, self.bias_hh,)
+
+    def forward_function(self, input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
+        if input.is_cuda:
+            igates = F.linear(input, w_ih)
+            hgates = F.linear(hidden[0], w_hh)
+            state = fusedBackend.LSTMFused.apply
+            return state(igates, hgates, hidden[1]) if b_ih is None else state(igates, hgates, hidden[1], b_ih, b_hh)
+        hx, cx = hidden
+        gates = F.linear(input, w_ih, b_ih) + F.linear(hx, w_hh, b_hh)
+
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
+        if self.print == True and self.training == True:
+            print("Input gate: "+str(ingate.data.abs().mean().item()))
+            print("Forget gate: "+str(forgetgate.abs().data.mean().item()))
+            print("Cell gate: "+str(cellgate.data.abs().mean().item()))
+            print("Output gate: "+str(outgate.data.abs().mean().item()))
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * F.tanh(cy)
+
+        return hy, cy
